702eefc26e7e20bc3141453e3c081262a12d01fc
cjneo <cjneo@mail.ustc.edu.cn>
2016-02-24 23:22:59 +0800

    new file:   checkimage.cpp 	new file:   include/checkImage.h

---
+++ include/checkImage.h
@@ -0,0 +1,10 @@
+#include<GL/glut.h>
+void makeCheckImage(void);
+#define checkImageWidth 64
+#define checkImageHeight 64
+
+void makeCheckImage(void);
+
+void ciinit(void);
+void cidisplay(void);
+
